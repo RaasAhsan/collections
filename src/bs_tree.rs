49752a99f04056c20bd89945847f1adfc658f8ec
Raas Ahsan <raas.ahsan@gmail.com>
2022-09-03 23:03:20 -0500

    Rename tree to BSTree

--- src/bs_tree.rs
+++ src/bs_tree.rs
@@ -1,54 +1,54 @@
 use std::cmp::Ordering;
 
 #[derive(Debug)]
-pub enum AVLTree<A> {
+pub enum BSTree<A> {
     Node {
         value: A,
-        left: Box<AVLTree<A>>,
-        right: Box<AVLTree<A>>,
+        left: Box<BSTree<A>>,
+        right: Box<BSTree<A>>,
     },
     Nil,
 }
 
-impl<A> AVLTree<A> {
+impl<A> BSTree<A> {
     pub fn new() -> Self {
-        AVLTree::Nil
+        BSTree::Nil
     }
 }
 
-impl<A> Default for AVLTree<A> {
+impl<A> Default for BSTree<A> {
     fn default() -> Self {
-        AVLTree::Nil
+        BSTree::Nil
     }
 }
 
-impl<A> AVLTree<A>
+impl<A> BSTree<A>
 where
     A: Ord,
 {
-    pub fn search(&self, a: A) -> Option<&AVLTree<A>> {
+    pub fn search(&self, a: A) -> Option<&BSTree<A>> {
         match self {
-            AVLTree::Node { value, left, right } => match a.cmp(value) {
+            BSTree::Node { value, left, right } => match a.cmp(value) {
                 Ordering::Less => left.search(a),
                 Ordering::Equal => Some(&self),
                 Ordering::Greater => right.search(a),
             },
-            AVLTree::Nil => None,
+            BSTree::Nil => None,
         }
     }
 
     pub fn insert(&mut self, a: A) -> bool {
         match self {
-            AVLTree::Node { value, left, right } => match a.cmp(value) {
+            BSTree::Node { value, left, right } => match a.cmp(value) {
                 Ordering::Less => left.insert(a),
                 Ordering::Equal => true,
                 Ordering::Greater => right.insert(a),
             },
-            AVLTree::Nil => {
-                *self = AVLTree::Node {
+            BSTree::Nil => {
+                *self = BSTree::Node {
                     value: a,
-                    left: Box::new(AVLTree::Nil),
-                    right: Box::new(AVLTree::Nil),
+                    left: Box::new(BSTree::Nil),
+                    right: Box::new(BSTree::Nil),
                 };
                 false
             }
@@ -57,37 +57,37 @@ where
 
     pub fn is_node(&self) -> bool {
         match self {
-            AVLTree::Node {
+            BSTree::Node {
                 value: _,
                 left: _,
                 right: _,
             } => true,
-            AVLTree::Nil => false,
+            BSTree::Nil => false,
         }
     }
 
     pub fn remove(&mut self, a: A) -> bool {
         match self {
-            AVLTree::Node { value, left, right } => match a.cmp(value) {
+            BSTree::Node { value, left, right } => match a.cmp(value) {
                 Ordering::Less => left.remove(a),
                 Ordering::Equal => {
                     match (left.is_node(), right.is_node()) {
                         (true, true) => right.swap_leftmost(value),
                         (true, false) => *self = std::mem::take(left),
                         (false, true) => *self = std::mem::take(right),
-                        (false, false) => *self = AVLTree::Nil,
+                        (false, false) => *self = BSTree::Nil,
                     }
                     true
                 }
                 Ordering::Greater => right.remove(a),
             },
-            AVLTree::Nil => false,
+            BSTree::Nil => false,
         }
     }
 
     fn swap_leftmost(&mut self, to: &mut A) {
         match self {
-            AVLTree::Node { value, left, right } => {
+            BSTree::Node { value, left, right } => {
                 if !left.is_node() {
                     std::mem::swap(value, to);
                     *self = std::mem::take(right);
@@ -95,29 +95,29 @@ where
                     left.swap_leftmost(to);
                 }
             }
-            AVLTree::Nil => {}
+            BSTree::Nil => {}
         }
     }
 
     pub fn height(&self) -> usize {
         match self {
-            AVLTree::Node {
+            BSTree::Node {
                 value: _,
                 left,
                 right,
             } => std::cmp::max(left.height(), right.height()),
-            AVLTree::Nil => 0,
+            BSTree::Nil => 0,
         }
     }
 
     pub fn balance(&self) -> i16 {
         match self {
-            AVLTree::Node {
+            BSTree::Node {
                 value: _,
                 left,
                 right,
             } => (right.height() as i16) - (left.height() as i16),
-            AVLTree::Nil => 0,
+            BSTree::Nil => 0,
         }
     }
 
@@ -129,7 +129,7 @@ where
 
 #[derive(Debug)]
 pub struct Iter<'a, A> {
-    tree: &'a AVLTree<A>,
+    tree: &'a BSTree<A>,
 }
 
 impl<'a, A> Iterator for Iter<'a, A> {
@@ -142,11 +142,11 @@ impl<'a, A> Iterator for Iter<'a, A> {
 
 #[cfg(test)]
 mod test {
-    use super::AVLTree;
+    use super::BSTree;
 
     #[test]
     fn tree_search() {
-        let mut tree = AVLTree::new();
+        let mut tree = BSTree::new();
         tree.insert(3);
         assert!(tree.search(3).is_some());
     }
