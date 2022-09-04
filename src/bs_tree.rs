d6c3f84466e1518d8463cdd370de2fb2370e8955
Raas Ahsan <raas.ahsan@gmail.com>
2022-09-04 00:06:42 -0500

    Tree iteration

--- src/bs_tree.rs
+++ src/bs_tree.rs
@@ -1,5 +1,6 @@
 use std::cmp::Ordering;
 
+/// An unbalanced binary search tree.
 #[derive(Debug)]
 pub enum BSTree<A> {
     Node {
@@ -72,10 +73,10 @@ where
                 Ordering::Less => left.remove(a),
                 Ordering::Equal => {
                     match (left.is_node(), right.is_node()) {
-                        (true, true) => right.swap_leftmost(value),
-                        (true, false) => *self = std::mem::take(left),
-                        (false, true) => *self = std::mem::take(right),
-                        (false, false) => *self = BSTree::Nil,
+                        (true, true) => right.swap_leftmost(value), // Swap the current node with its immediate successor
+                        (true, false) => *self = std::mem::take(left), // Promote the left subtree
+                        (false, true) => *self = std::mem::take(right), // Promote the right subtree
+                        (false, false) => *self = BSTree::Nil,      // Clear out the current node
                     }
                     true
                 }
@@ -121,22 +122,81 @@ where
         }
     }
 
-    /// Returns an iterator that traverses the keys of the tree in ascending order
-    pub fn iter<'a>(&self) -> Iter<'a, A> {
-        todo!()
+    pub fn value(&self) -> Option<&A> {
+        match self {
+            BSTree::Node {
+                value,
+                left: _,
+                right: _,
+            } => Some(value),
+            BSTree::Nil => None,
+        }
+    }
+
+    /// Returns an iterator that traverses the keys of the tree in ascending order.
+    /// This corresponds to an in-order traveral of the tree.
+    pub fn iter<'a>(&'a self) -> Iter<'a, A> {
+        Iter {
+            state: IterState::Left,
+            tree: self,
+            parent: None,
+        }
     }
 }
 
 #[derive(Debug)]
 pub struct Iter<'a, A> {
+    state: IterState,
     tree: &'a BSTree<A>,
+    parent: Option<Box<Iter<'a, A>>>,
 }
 
-impl<'a, A> Iterator for Iter<'a, A> {
+#[derive(Debug)]
+enum IterState {
+    Left,
+    Node,
+    Right,
+}
+
+impl<'a, A> Iterator for Iter<'a, A>
+where
+    A: Ord,
+{
     type Item = &'a A;
 
     fn next(&mut self) -> Option<Self::Item> {
-        todo!()
+        match self.tree {
+            BSTree::Node { value, left, right } => match self.state {
+                IterState::Left => {
+                    self.state = IterState::Node;
+                    let mut new_parent = left.iter();
+                    std::mem::swap(self, &mut new_parent);
+                    self.parent = Some(Box::new(new_parent));
+                    self.next()
+                }
+                IterState::Node => {
+                    self.state = IterState::Right;
+                    let mut new_parent = right.iter();
+                    std::mem::swap(self, &mut new_parent);
+                    self.parent = Some(Box::new(new_parent));
+                    Some(value)
+                }
+                IterState::Right => match self.parent.take() {
+                    Some(mut p) => {
+                        std::mem::swap(self, &mut p);
+                        self.next()
+                    }
+                    None => None,
+                },
+            },
+            BSTree::Nil => match self.parent.take() {
+                Some(mut p) => {
+                    std::mem::swap(self, &mut p);
+                    self.next()
+                }
+                None => None,
+            },
+        }
     }
 }
 
@@ -172,5 +232,23 @@ mod test {
         tree.insert(1);
         tree.insert(0);
         assert_eq!(tree.height(), 6);
+    }
+
+    #[test]
+    fn tree_iteration() {
+        let mut tree = BSTree::new();
+        tree.insert(4);
+        tree.insert(3);
+        tree.insert(5);
+        tree.insert(0);
+        tree.insert(2);
+        tree.insert(1);
+        let mut iter = tree.iter();
+        assert_eq!(iter.next(), Some(&0));
+        assert_eq!(iter.next(), Some(&1));
+        assert_eq!(iter.next(), Some(&2));
+        assert_eq!(iter.next(), Some(&3));
+        assert_eq!(iter.next(), Some(&4));
+        assert_eq!(iter.next(), Some(&5));
     }
 }
